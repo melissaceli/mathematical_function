28c6df9e6a2ad986468544b7380b4cc74eca8cfc
Melissa Celi <melissaceli71@gmail.com>
2024-03-27 14:52:02 +0100

    Update test_rectangle

--- tests/polygons/test_rectangle.py
+++ tests/polygons/test_rectangle.py
@@ -10,16 +10,3 @@ class RectangleTest(unittest.TestCase):
         rectangle = Rectangle(base=base, height=height)
         self.assertEqual(round(float(base * height), 2), rectangle.area())
         self.assertEqual((base + height) * 2, rectangle.perimeter())
-
-    '''
-    def test_rectangle_not_valid(self):
-        with self.assertRaises(Exception) as context:
-            RectangleValidated(base=-3, height=10)
-        self.assertTrue("Input should be greater than 0" in str(context.exception))
-
-        with self.assertRaises(Exception) as context:
-            RectangleValidated(base=3, height=-10)
-        self.assertTrue("Input should be greater than 0" in str(context.exception))
-    '''
-
-
